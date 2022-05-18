32551848c439de5db640029e0166382b23f57a93
Hamza <boujahamza@gmail.com>
2022-05-18 18:55:29 +0100

    added diskstorage to multer

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const multer = require('multer');
-
-var upload = multer();
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 port = process.env.PORT || 4001;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'files/');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + JSON.parse(req.headers["user"]).user_id;
+      cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+  })
+  
+const upload = multer({storage: storage});
 
 app.get("/", (req, res) => {
     console.log("recieved request");
 });
 
-app.post("/", upload.single("data"),(req, res) => {
+app.post("/", upload.single("data"), (req, res, next) => {
+    
     console.log("received file");
-    console.log(req.file);
+    console.log(JSON.parse(req.headers["user"]).user_id);
+
+    res.status(200).send();
 })
 
 app.listen(port, () => {
